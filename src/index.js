f2394b3ad9b0604b432e924208a616601867b932
Anton Honcharov <antonhoncharovv@gmail.com>
2022-04-01 21:35:47 +0300

    add some src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-import './sass/main.scss';
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+const capital = "ukraine";
+
+
+
+fetchCountries(capital).then(data => {
+    console.log(data[0])
+});
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+        return response.json();
+    });   
+}
+
+// https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages
+
+// fetch(`https://restcountries.com/v3.1/name/${capital}`).then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data[0]);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+
+// fetch('https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages').then(response => {
+//     return response.json();
+// }).then(country => {
+//     console.log(country)
+// });
